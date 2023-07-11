f8e420c5706e960a940f2e1577b7034e55bf2047
Palathingal Frank <palafrank@outlook.com>
2023-07-11 21:05:08 +0000

    Initial commit

---
+++ compare.py
@@ -0,0 +1,90 @@
+import yfinance as yf
+import pandas as pd
+import sys
+
+TICKER = "symbol"
+# Fundamentals
+# Income
+MARKETCAP = "marketCap"
+REVENUE = "totalRevenue"
+GROSS = "grossProfits"
+EBITDA = "ebitda"
+NET_INCOME = "netIncomeToCommon"
+
+# Assets
+CASH = "totalCash"
+CURRENT_RATIO = "currentRatio"
+DEBT = "totalDebt"
+DEBT_TO_EQUITY = "debtToEquity"
+
+# Cash Flow
+FCF = "freeCashflow"
+OP_CF = "operatingCashflow"
+PAYOUT_RATIO = "payoutRatio"
+
+# Metrics
+GROSS_MARGIN = "grossMargins"
+OP_MARGIN = "operatingMargins"
+REV_GROWTH = "revenueGrowth"
+EARNING_GROWTH = "earningsGrowth"
+
+# Returns
+ROA = "returnOnAssets"
+ROE = "returnOnEquity"
+TRAILING_PE = "trailingPE"
+FORWARD_PE = "forwardPE"
+
+
+class comparator:
+    cols = [
+        TICKER,
+        MARKETCAP,
+        REVENUE,
+        GROSS,
+        EBITDA,
+        NET_INCOME,
+        CASH,
+        CURRENT_RATIO,
+        DEBT,
+        DEBT_TO_EQUITY,
+        FCF,
+        OP_CF,
+        PAYOUT_RATIO,
+        GROSS_MARGIN,
+        OP_MARGIN,
+        REV_GROWTH,
+        EARNING_GROWTH,
+        ROA,
+        ROE,
+        TRAILING_PE,
+        FORWARD_PE,
+    ]
+
+    def __init__(self, tickers):
+        self.data = pd.DataFrame(
+            columns=self.cols,
+        )
+        for ticker in tickers:
+            info = yf.Ticker(ticker)
+            loc = len(self.data)
+            for item in self.cols:
+                try:
+                    self.data.at[loc, item] = info.info[item]
+                except:
+                    self.data.at[loc, item] = "--"
+
+    def info(self):
+        return self.data
+
+
+num_params = len(sys.argv)
+if num_params == 1:
+    sys.exit("no ticker(s) provided for data collection")
+tickers = []
+for i in range(1, num_params):
+    print(sys.argv[i])
+    tickers.append(sys.argv[i])
+print(tickers)
+comp = comparator(tickers)
+# with pd.option_context("display.max_rows", None, "display.max_columns", None):
+print(comp.info())
