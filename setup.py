64ff3f35fb75fd9bc66088635cc9baef1b4555c9
Palathingal Frank <palafrank@outlook.com>
2023-07-17 14:37:34 -0700

    Add setup file with dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Package configuration."""
+from setuptools import find_packages, setup
+
+setup(
+    name="stockviewer",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+          'yfinance',
+          'numpy',
+          'bs4',
+          'pandas'
+      ]
+    )
